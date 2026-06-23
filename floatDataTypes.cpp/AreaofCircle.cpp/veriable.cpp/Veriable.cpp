9a76207f7aef134f82197eb8e0561dbdee85a3a8
shreyashdubal5-stack <shreyashdubal5@gmail.com>
2026-06-23 15:14:33 +0530

    veriables

---
+++ floatDataTypes.cpp/AreaofCircle.cpp/veriable.cpp/Veriable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //int age = 20;
+    //cout<<age;
+   //  return 0;
+
+   int age = 20;
+
+   cin>>age;
+
+   cout<<age;
+return 0;
+
+}
