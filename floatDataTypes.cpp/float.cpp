533b4196e5d688bf803fe86846fd7237571011ed
shreyashdubal5-stack <shreyashdubal5@gmail.com>
2026-06-26 13:33:43 +0530

    ifelse

--- floatDataTypes.cpp/float.cpp
+++ floatDataTypes.cpp/float.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    //float x= 5;
-    //float y= 3;
-
+    int a = 90;
+     int b = 92;
+      int c = 93;
+       int d = 95;
+        int e = 98;
+        
+        int percentage = (a+b+c+d+e)/5;
+        cout<<percentage;
+}
    
---
+++ ifElse.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+     int n;
+
+    cout<<"Enter a number:";
+    cin>>n;
+
+if(n%2==0){
+     cout<<"even number";
+}
+
+if(n%2!=0){
+    cout<<"odd number";
+}
+ return 0;
+
+}
+
